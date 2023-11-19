b95e14b1e63488a9db6e4f41c05769a9fe79ac18
jonatw <me@jona.tw>
2023-11-19 09:01:59 +0800

    fix watermark remove bugs

--- remove_wartermark.py
+++ remove_wartermark.py
@@ -1,23 +1,34 @@
-import fitz
+import fitz, re
 
 def remove_watermark(input_file, output_file):
     doc = fitz.open(input_file)
-    def most_frequent_substring(byte_array, length):
+    def most_frequent_substring_with_pattern(byte_array, pattern, length):
         count = {}
-        for i in range(len(byte_array) - length + 1):
-            substring = bytes(byte_array[i:i + length])
-            if substring in count:
-                count[substring] += 1
+        pattern_length = len(pattern)
+        i = 0
+
+        while i < len(byte_array) - pattern_length:
+            # Find the occurrence of the pattern
+            if byte_array[i:i + pattern_length] == pattern:
+                # Extract the substring of the desired length after the pattern
+                substring = bytes(byte_array[i:i + pattern_length + length])
+                # Count the frequency
+                count[substring] = count.get(substring, 0) + 1
+                # Move past this occurrence
+                i += pattern_length
             else:
-                count[substring] = 1
+                i += 1
+
+        # Find the most frequent substring
         most_frequent = max(count, key=count.get)
         return most_frequent, count[most_frequent]
     page = doc[0]
     page.clean_contents()
     xref = page.get_contents()[0]
     cont = bytearray(page.read_contents())
-    byte_length = 100
-    most_frequent, frequency = most_frequent_substring(cont, byte_length)
+    pattern = b" Td\n<"
+    length = 100    
+    most_frequent, frequency = most_frequent_substring_with_pattern(cont, pattern, length)
     for page in doc:
         page.clean_contents()
         xref = page.get_contents()[0]
